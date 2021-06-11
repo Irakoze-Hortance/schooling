719388b8c887a9dc6fef7565496aa429132ec9a6
Irakoze Hortance <54577367+Irakoze-Hortance@users.noreply.github.com>
2021-06-11 09:42:55 -0700

    setters and getters

--- src/main/java/com/example/schooling/models/Student.java
+++ src/main/java/com/example/schooling/models/Student.java
@@ -1,2 +1,56 @@
-package com.example.schooling.models;public class Student {
+package com.example.schooling.models;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+@Entity
+@Table
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+
+
+    public Student(String firstName, String lastName, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+    public Student(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
