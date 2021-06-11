aff5d9aca12404dc02bce8b4a94d483a307aa3f5
Irakoze Hortance <54577367+Irakoze-Hortance@users.noreply.github.com>
2021-06-11 09:42:09 -0700

    student repository

--- src/main/java/com/example/schooling/repositories/StudentRepository.java
+++ src/main/java/com/example/schooling/repositories/StudentRepository.java
@@ -1,2 +1,22 @@
-package com.example.schooling.repositories;public interface StudentRepository {
+package com.example.schooling.repositories;
+
+import com.example.schooling.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+    //query by dynamic finders
+    List<Student> findBFirstName(String firstName);
+    List<Student> findByFirstNameAndLastName(String firstName,String lastName);
+    List<Student> findByFirstNameOrLastName(String firstName,String lastName);
+
+    //query by sql
+    @Query(value = "")
+    List<Student> findByFirstNameAndLastNameHQL(String firstName,String lastName);
+    @Query(value = "select * from student stb where  stb.first_name=:firstNameParam and stb.last_name=lastNameParam" , nativeQuery=true)
+    List<Student> findByFirstNameAndLastNameSQL(@Param("firstNameParam")  String firstName, @Param("lastNameParam") String lastName);
 }
