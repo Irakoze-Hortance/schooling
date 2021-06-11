1ac5bd9a816c046754dd52f745ff4f5c4ad57006
Irakoze Hortance <54577367+Irakoze-Hortance@users.noreply.github.com>
2021-06-11 08:22:45 -0700

    initial commit

---
+++ src/main/java/com/example/schooling/SchoolingApplication.java
@@ -0,0 +1,13 @@
+package com.example.schooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolingApplication.class, args);
+    }
+
+}
